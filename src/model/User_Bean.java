fcff35866bdba76827e9d8e0310817773c741221
NiumalAtTCG <140277639+NiumalAtTCG@users.noreply.github.com>
2025-08-19 00:28:24 +0530

    1st save

---
+++ src/model/User_Bean.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author User
+ */
+public class User_Bean {
+   private int id;
+   private int RoleId;
+    private String username;
+    private String fname;
+    private String lname; 
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the fname
+     */
+    public String getFname() {
+        return fname;
+    }
+
+    /**
+     * @param fname the fname to set
+     */
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    /**
+     * @return the lname
+     */
+    public String getLname() {
+        return lname;
+    }
+
+    /**
+     * @param lname the lname to set
+     */
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    /**
+     * @return the RoleId
+     */
+    public int getRoleId() {
+        return RoleId;
+    }
+
+    /**
+     * @param RoleId the RoleId to set
+     */
+    public void setRoleId(int RoleId) {
+        this.RoleId = RoleId;
+    }
+    
+    
+}
